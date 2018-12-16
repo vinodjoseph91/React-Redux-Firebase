b4c631ecdd474a27bc7fb38ee242c1c51f215703
vinod.joseph <vinod.joseph@target.com>
2018-12-16 19:10:39 +0530

    Added Project summary component and using it in ProjectList.

--- sample-project-manager/src/components/projects/ProjectList.js
+++ sample-project-manager/src/components/projects/ProjectList.js
@@ -1,33 +1,14 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary';
 
 const ProjectList = () => {
 
     return (
         <div className="project-list section">
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">First Title</span>
-                    <p>Posted by some user</p>
-                    <p className="grey-text">16th December , 6:30 pm</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Second Title</span>
-                    <p>Posted by some user</p>
-                    <p className="grey-text">16th December , 6:40 pm</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Third Title</span>
-                    <p>Posted by some user</p>
-                    <p className="grey-text">16th December , 6:45 pm</p>
-                </div>
-            </div>
-
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
         </div>
     );
 
---
+++ sample-project-manager/src/components/projects/ProjectSummary.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+
+    return (
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text text-darken-3">
+                <span className="card-title">First Title</span>
+                <p>Posted by some user</p>
+                <p className="grey-text">16th December , 6:30 pm</p>
+            </div>
+        </div>
+    );
+}
+
+export default ProjectSummary;
