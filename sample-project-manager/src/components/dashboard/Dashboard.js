ffa901c154c9aa18d853ac113a7556c2f9807be0
vinod.joseph <vinod.joseph@target.com>
2018-12-16 18:41:27 +0530

    Added Project List Component

--- sample-project-manager/src/components/dashboard/Dashboard.js
+++ sample-project-manager/src/components/dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Notifications from './Notifications';
-
+import ProjectList from '../projects/ProjectList';
 class Dashboard extends Component {
 
     render() {
@@ -8,7 +8,9 @@ class Dashboard extends Component {
         return (
             <div className="dashboard container">
                 <div className="row">
-                    <div className="col s12 m6"></div>
+                    <div className="col s12 m6">
+                        <ProjectList />
+                    </div>
                     <div className="col s12 m5 offset-m1">
                         <Notifications />
                     </div>
