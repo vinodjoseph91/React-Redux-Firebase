15712e7bbd42eb59a813f1c68c61d88282425138
vinod.joseph <vinod.joseph@target.com>
2018-12-17 23:34:27 +0530

    Using the dummy project data to render in the dashboard ui component

--- sample-project-manager/src/components/projects/ProjectSummary.js
+++ sample-project-manager/src/components/projects/ProjectSummary.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
 
     return (
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-text text-darken-3">
-                <span className="card-title">First Title</span>
+                <span className="card-title">{project.title}</span>
                 <p>Posted by some user</p>
                 <p className="grey-text">16th December , 6:30 pm</p>
             </div>
